363c8426125c77c4058b5eec5702e9b6bbe2bb50
Michael Villarreal <mvillarreal0605@gmail.com>
2020-08-24 16:11:52 -0500

    created the other routes for user relay registrations

--- app/controllers/api/v1/user_relay_registrations_controller.rb
+++ app/controllers/api/v1/user_relay_registrations_controller.rb
@@ -1,5 +1,49 @@
-class Api::V1::RestaurantsController < Api::V1::BaseController
+class Api::V1::UserRelayRegistrationsController < Api::V1::BaseController
+  before_action :set_urr, only: [ :show, :update ]
+
+
   def index
     @user_relay_registrations = UserRelayRegistration.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @urr = UserRelayRegistration.new(urr_params)
+    if @urr.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def update
+    if @urr.update(urr_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @urr.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_urr
+    @urr = UserRelayRegistration.find(params[:id])
+  end
+
+  def urr_params
+    params.require(:user_relay_registration).permit(:id, :user_id, :device_guid, :device_description, :usage_count, :create_time, :update_time)
+  end
+
+  def render_error
+    render json: { errors: @urr.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_ids = ["1", "2", "3", "4", "5", "6","7", "8", "9", "10"]
+
+5.times do
+  UserRelayRegistration.create!({
+    user_id: user_ids.sample,
+    device_guid: "test_guid",
+    device_description: "this is a test description",
+    usage_count: [1..50].sample,
+    create_time: Time.zone.now,
+    update_time: Time.zone.now
+  })
+end
