efbbbeab025337b05e91cca8d25873b6f1ef8cca
Michael Villarreal <mvillarreal0605@gmail.com>
2020-11-24 22:49:20 -0600

    Put up the banner.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :user_relay_registrations, only: [ :index, :show, :update, :create, :destroy ]
@@ -9,7 +11,4 @@ Rails.application.routes.draw do
       resources :sessions, only: [ :index, :show, :update, :create, :destroy ]
     end
   end
-
-
-
 end
