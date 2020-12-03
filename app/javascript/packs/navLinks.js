f9c0b075a35a6d5e2850a23c5cbc23595023bc6e
Michael Villarreal <mvillarreal0605@gmail.com>
2020-12-03 17:30:44 -0600

    General layout is set up

---
+++ app/javascript/packs/navLinks.js
@@ -0,0 +1,17 @@
+const navLinks = () => {
+
+  const navbarLinks = document.querySelectorAll('.nav_list_link');
+
+  navbarLinks.forEach((link) => {
+   link.addEventListener('click', () => {
+     event.preventDefault();
+     let section = document.querySelector(link.childNodes[1].getAttribute('href'));
+     section.scrollIntoView({
+       behavior: 'smooth',
+       block: 'start'
+     });
+   })
+  })
+}
+
+navLinks();
