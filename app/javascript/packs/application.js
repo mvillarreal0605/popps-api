87ab73b65d9f5174e038faca8fe8115e7aa07d22
Michael Villarreal <mvillarreal0605@gmail.com>
2021-06-08 23:31:02 -0500

    Works in progress.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require("bootstrap")
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,3 +16,16 @@ require('jquery')
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import "flickity";
+import "font-awesome/scss/font-awesome.scss";
+require("isotope/src/isotope.scss");
+require("isotope/src/marvel.css");
+import "lightbox";
+import "lightbox/assets/styles/main.scss";
+import "lightbox/assets/fontello/css/glyph.css";
+import "parallax";
+require("../vendor/alerts/alert");
+require("../vendor/contact/contact");
+require("../vendor/reservation/reservation");
+require("../src/theme");
+require("../src/theme.min");
